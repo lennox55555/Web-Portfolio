22165b97766f6b07852285bec592aa2873bcefe4
Lennox Anderson <lennox55555@icloud.com>
2022-07-10 15:45:38 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let elementa = document.getElementById('project-1');
+elementa.addEventListener('click', () => {
+    elementa.classList.add('active');
+    elementa.classList.add('project-1')
+});
+
+let elementb = document.getElementById('project-2');
+elementb.addEventListener('click', () => {
+
+    elementb.classList.add('active');
+});
+
+let elementc = document.getElementById('project-3');
+elementc.addEventListener('hover', () => {
+
+    elementc.classList.add('active');
+});
+
+let elementd = document.getElementById('project-4');
+elementd.addEventListener('click', () => {
+
+    elementd.classList.add('active');
+});
+
+let elemente = document.getElementById('project-5');
+elemente.addEventListener('click', () => {
+
+    elemente.classList.add('active');
+});
+
+let elementf = document.getElementById('project-6');
+elementf.addEventListener('click', () => {
+
+    elementf.classList.add('active');
+});
